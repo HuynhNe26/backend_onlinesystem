9cec2ed103c4d4bd95b3658a50e568707aa837e2
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-11-13 12:16:13 +0700

    update

--- back_end/config/db_config.py
+++ back_end/config/db_config.py
@@ -1,16 +1,27 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 
 def get_db_connection():
     try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Huynh@2608",
-            database="online_testing"
-        )
+        if os.environ.get('RENDER') == "true":
+            # MySQL trên Render
+            conn = mysql.connector.connect(
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASS"),
+                database=os.getenv("DB_NAME"),
+                port=os.getenv("DB_PORT", 3306)  # mặc định port 3306
+            )
+        else:
+            # Local MySQL
+            conn = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="Huynh@2608",
+                database="online_testing"
+            )
         return conn
     except Error as e:
-        print(f"Lỗi kết nối: {e}")
+        print(f"Lỗi kết nối MySQL: {e}")
         return None
-
