15c38c0f71888e25f5c1c703632d926e91d23337
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-11-13 08:41:48 +0700

    update

--- back_end/route/users/category.py
+++ back_end/route/users/category.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, jsonify
-from flask_jwt_extended import jwt_required
 from ...config.db_config import get_db_connection
 
 category_bp = Blueprint('category_bp', __name__)
 
 @category_bp.route('/categories', methods=['GET'])
-@jwt_required()
 def get_all_categories():
     try:
         conn = get_db_connection()
